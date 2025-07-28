ac9d5cb8c39fb3d1f4df865022cedcecee6ccfdf
Orest Hera <orest.hera@gmail.com>
2025-07-29 01:51:46 +0300

    cuDSS: remove unused parse functions

--- ParallelJacobian/Include/NewtonSolverCuDSS.h
+++ ParallelJacobian/Include/NewtonSolverCuDSS.h
@@ -7,8 +7,6 @@ class NewtonSolverCuDSS {
 private:
 	DataInitializerCuDSS* data;
 	const Settings::SettingsData& settings_;
-	int count_non_zero_elements(double* matrix_A);
-	void parse_to_csr(int* csr_cols, int* csr_rows, double* csr_values, double* matrix_A);
 public:
 
 	NewtonSolverCuDSS(DataInitializerCuDSS*data, const Settings::SettingsData& settings);
